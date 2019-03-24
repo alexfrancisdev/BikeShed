99cbd328ee543eae8c7cd826539ed34f714e2ff6
AlexFrancis95 <alexfrancis@email.com>
2019-03-24 20:42:20 +0000

    added navbar

--- src/components/common/navbar.js
+++ src/components/common/navbar.js
@@ -1,18 +1,43 @@
 import React from 'react'
+import PropTypes from 'prop-types'
+import { withStyles } from '@material-ui/core/styles'
 import AppBar from '@material-ui/core/AppBar'
 import Toolbar from '@material-ui/core/Toolbar'
+import Typography from '@material-ui/core/Typography'
+import Button from '@material-ui/core/Button'
 
-const NavBar = () => {
-  return(
-    <div>
+const styles = {
+  root: {
+    flexGrow: 1
+  },
+  grow: {
+    flexGrow: 1
+  },
+  menuButton: {
+    marginLeft: -12,
+    marginRight: 20
+  }
+}
+
+function NavBar(props) {
+  const { classes } = props
+  return (
+    <div className={classes.root}>
       <AppBar position="static">
         <Toolbar>
-          <h1>
-    React Material UI Example
-          </h1>
+          <Typography variant="h6" color="inherit" className={classes.grow}>
+            Bikeshed
+          </Typography>
+          <Button color="inherit">Login</Button>
+          <Button color="inherit">Sign Up</Button>
         </Toolbar>
       </AppBar>
     </div>
   )
 }
-export default NavBar
+
+NavBar.propTypes = {
+  classes: PropTypes.object.isRequired
+}
+
+export default withStyles(styles)(NavBar)
