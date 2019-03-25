1e93e304d7742ebbc809ec6a4006a9ab0291c77a
AlexFrancis95 <alexfrancis@email.com>
2019-03-25 11:30:30 +0000

    added libary of common functions

--- src/lib/common.js
+++ src/lib/common.js
@@ -6,3 +6,36 @@ export function handleChange(event) {
 export function saveToken(token) {
   localStorage.setItem('bikeshed-token', token)
 }
+
+export function getToken() {
+  return localStorage.getItem('bikeshed-token')
+}
+
+export function decodeToken() {
+  const token = getToken()
+  if (!token) return {}
+  const decoded = JSON.parse(atob(token.split('.')[1]))
+  return decoded
+}
+
+export function tokenUsername() {
+  return decodeToken().username
+}
+
+export function tokenUserId() {
+  return decodeToken().sub
+}
+
+export function deleteToken() {
+  localStorage.removeItem('project4-token')
+}
+
+export function isAuthenticated() {
+  return !!getToken()
+}
+
+export function authorizationHeader() {
+  return {
+    headers: { Authorization: 'Bearer ' + getToken() }
+  }
+}
